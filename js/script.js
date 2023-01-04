b292e16dbdd815237f5e8793d529a0bcff1ed4e0
Julius Joshua <121971090+Jules-infotech@users.noreply.github.com>
2023-01-04 22:54:39 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+ 
+ const navBtn= document.querySelector('#navbar-toggler');
+ const navDiv= document.querySelector('.navbar-collapse');
+ 
+navBtn.addEventListener('click',()=>{
+    navDiv.classList.toggle('showNav');
+});
+
+//stopping animation ans transition duting window resizing
+let resizeTimer;
+window.addEventListener('resize', () => {
+    document.body.classList.add('resize-animation-stopper');
+    clearTimeout(resizeTimer);
+    resizeTimer =setTimeout(()=> {
+        document.body.classList.remove
+        ('resize-animation-stopper')
+    }, 400);
+});
